23a459e5566edae71b981e317acc81ba5def204c
Victor Hugo <120677166+VictorH456@users.noreply.github.com>
2023-11-01 15:12:27 -0400

    Combate

--- Rpg/Main.java
+++ Rpg/Main.java
@@ -17,6 +17,7 @@ public class Main {
         String nome;
         boolean gameover = true;
         Random random = new Random();
+        boolean batalhaOn = true;
 
         //Seleção de raça e trilha (classe)
         System.out.println("Escolha uma raça[1 a 3]: ");
@@ -80,14 +81,13 @@ public class Main {
             System.out.println("Selecione uma opção[1 a 5]: ");
             System.out.println("1. Batalha\n2. Bolsa\n3. Subir andar\n4. Descer andar\n5. Descansar");
 
-            inimigo = new Inimigo(racaInimigo,random.nextInt(personagem.getAndar()-1, personagem.getAndar()+2));
             escolhaValida = false;
             escolha = 0;
 
             while (!escolhaValida) {
                 try {
                     escolha = input.nextInt();
-                    if (escolha >= 1 && escolha <= 3) {
+                    if (escolha >= 1 && escolha <= 5) {
                         escolhaValida = true;
                     } else {
                         System.out.println("Escolha inválida. Digite novamente.");
@@ -96,14 +96,56 @@ public class Main {
                     System.out.println("Entrada inválida. Digite novamente.");
                 }
             }
-            if (escolha == 1){
 
-                System.out.printf("%s\t Level: %d\n\n", inimigo.getRaca(), inimigo.getLevel());
-                System.out.printf("%s\t Level: %d\n", personagem.getRaca(), personagem.getLevel());
-            } else if (escolha == 2) {
+            if (escolha == 1){
+                inimigo = new Inimigo(racaInimigo, random.nextInt(personagem.getAndar()-1, personagem.getAndar()+2));
+                while (batalhaOn) {
+
+                    System.out.printf("%s\t     hp:%.2f/%.2f    Level: %d\n\n", inimigo.getRaca(), inimigo.getVidaAtual(), inimigo.getVidaMAx(), inimigo.getLevel());
+                    System.out.printf("%s\t     hp:%.2f/%.2f    Level: %d\n\n", personagem.getNome(), personagem.getVidaAtual(), personagem.getVidaMAx(), personagem.getLevel());
+
+                    if (personagem.getVelocidadeAtual() >= inimigo.getVelocidadeAtual()){
+                        escolhaValida = false;
+                        System.out.println("Selecione uma opção[1 a 4]");
+                        System.out.println("1. Ataque leve\n2. Ataque normal\n3. Ataque pesado");
+                        while (!escolhaValida) {
+                            try {
+                                escolha = input.nextInt();
+                                if (escolha >= 1 && escolha <= 4) {
+                                    escolhaValida = true;
+                                } else {
+                                    System.out.println("Escolha inválida. Digite novamente.");
+                                }
+                            } catch (java.util.InputMismatchException e) {
+                                System.out.println("Entrada inválida. Digite novamente.");
+                            }
+                        }
+                        personagem.atacar(inimigo,escolha,random);
+                        personagem.setVelocidadeAtual(personagem.getVelocidadeAtual() - inimigo.getVelocidadeMax());
+                    }
+                    else if (personagem.getVelocidadeAtual() <= inimigo.getVelocidadeAtual()) {
+                        inimigo.atacar(personagem,random);
+                        inimigo.setVelocidadeAtual(inimigo.getVelocidadeAtual() - personagem.getVelocidadeMax());
+                    }
+
+                    if(inimigo.getVelocidadeAtual() == 0 && personagem.getVelocidadeAtual() == 0){
+                        inimigo.setVelocidadeAtual(inimigo.getVelocidadeMax());
+                        personagem.setVelocidadeAtual(personagem.getVelocidadeMax());
+                    }
+                    if(personagem.getVidaAtual() == 0 || inimigo.getVidaAtual() == 0){
+                        batalhaOn = false;
+                    }
+
+                }
+                } else if (escolha == 2){
                 gameover = false;
+            } else if (escolha == 3){
+                gameover = false;
+            } else if (escolha == 4){
+
+            }else {
+
             }
-            else {gameover = false;}
         }
 
     }
--- Rpg/Personagem.java
+++ Rpg/Personagem.java
@@ -1,5 +1,5 @@
 package Projetos.Rpg;
-
+import java.util.Random;
 abstract class Personagem {
 
     protected String raca;
@@ -19,14 +19,24 @@ abstract class Personagem {
     protected double getVelocidadeMax() {return velocidadeMax;}
     protected void setVelocidadeMax(double velocidadeMax) {this.velocidadeMax = velocidadeMax;}
     protected double getVelocidadeAtual() {return velocidadeAtual;}
-    protected void setVelocidadeAtual(double velocidadeMax) {this.velocidadeAtual = velocidadeMax;}
+    protected void setVelocidadeAtual(double velocidade) {
+        if (velocidade <= 0) {this.velocidadeAtual = 0;}
+        else {this.velocidadeAtual = velocidade;}
+    }
     protected double getStrength() {return strength;}
     protected void setStrength(double strength) {this.strength = strength;}
     protected double getVidaAtual(){return this.vidaAtual;}
 
-    protected void setVidaAtual(double vida) {this.vidaAtual = vida;}
+    protected void setVidaAtual(double vida) {
+        if (vida <= 0) {this.vidaAtual = 0;}
+        else {this.vidaAtual = vida;}
+    }
     protected double getVidaMAx(){return this.vidaMax;}
     protected void setVidaMax(double vidaMax) {this.vidaMax = vidaMax;}
+
+    protected void bolsa(){
+
+    }
 }
 
 class Heroi extends Personagem {
@@ -80,17 +90,68 @@ class Heroi extends Personagem {
     public int getAndar() {
         return this.andar;
     }
+
+
+    void atacar(Inimigo inimigo, int golpe,Random random) {
+        switch (golpe) {
+            case 1:
+                inimigo.setVidaAtual(inimigo.getVidaAtual() - strength / 3);
+                System.out.printf("%s Atacou leve\n",nome);
+                break;
+            case 2:
+
+                if (random.nextInt(0, 11) <= 7) {
+                    inimigo.setVidaAtual(inimigo.getVidaAtual() - strength / 2);
+                    System.out.printf("%s Atacou normalmente\n",nome);
+                }
+                else {System.out.printf("%s Falhou\n",nome);}
+                break;
+            default:
+                if (random.nextInt(0, 11) <= 5) {
+                    inimigo.setVidaAtual(inimigo.getVidaAtual() - strength);
+                    System.out.printf("%s Atacou Forte\n",nome);
+                }
+                else {System.out.printf("%s Falhou\n",nome);}
+                break;
+        }
+    }
 }
 class Inimigo extends Personagem{
     private double expDrop;
     Inimigo(Racas raca,int valor){
         setLevel(valor);
-        this.vidaMax += raca.getHpBase() * level * 0.26;
-        this.vidaAtual = getVidaMAx() * level * 0.26;
-        this.strength += raca.getStrengthBase() * level * 0.26;
-        this.velocidadeMax += raca.getSpeedBase() * level * 0.26;
+        this.vidaMax = raca.getHpBase() + raca.getHpBase() * level * 0.26;
+        this.vidaAtual = getVidaMAx();
+        this.strength = raca.getStrengthBase() + raca.getStrengthBase() * level * 0.26;
+        this.velocidadeMax = raca.getSpeedBase() + raca.getSpeedBase() * level * 0.26;
         this.raca = raca.getNome();
         this.expDrop = raca.getExpDrop() * this.level;
+    }
+
+
+    public void atacar(Heroi heroi, Random random) {
+        switch (random.nextInt(1,4)) {
+            case 1:
+                heroi.setVidaAtual(heroi.getVidaAtual() - strength / 3);
+                System.out.printf("%s Atacou leve\n",raca);
+                break;
+            case 2:
+                if (random.nextInt(0, 11) <= 7){
+                    heroi.setVidaAtual(heroi.getVidaAtual() - strength / 2);
+                    System.out.printf("%s Atacou Normalmente\n",raca);
+                } else {
+                    System.out.printf("%s Falhou\n",raca);
+                }
+                break;
+            default:
+                if (random.nextInt(0, 11) <= 5)
+                {
+                    heroi.setVidaAtual(heroi.getVidaAtual() - strength);
+                    System.out.printf("%s Atacou Forte\n",raca);
+                }
+                else {System.out.printf("%s Falhou\n",raca);}
+                break;
+        }
     }
 }
 
