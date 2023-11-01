e073d744861f4c46db758da8ecb9fde403c50246
Victor Hugo <120677166+VictorH456@users.noreply.github.com>
2023-11-01 09:46:09 -0400

    Modificação trilhas, inimigos e main

--- Rpg/Personagem.java
+++ Rpg/Personagem.java
@@ -1,50 +1,80 @@
 package Projetos.Rpg;
 
 abstract class Personagem {
-    String nome;
-    String raca;
-    String trilha;
-    double vida;
-    double strength;
-    double velocidade;
-    int level = 1;
-    double expAtual = 0;
-    double expUp = 10;
-    double expGanho = 2;
+
+    protected String raca;
+    protected double vidaAtual;
+    protected double vidaMax;
+    protected double strength;
+    protected double velocidade;
+    protected int level = 1;
     //Level
-    protected double getLevel(){return  level;}
+    protected int getLevel(){return  level;}
     protected void  setLevel(int level){this.level = level;}
-    protected double getExpAtual(){return  expAtual;}
-    protected void setExpAtual(double ExpAtual){this.expAtual = ExpAtual;}
-    protected double getExpUp(){return  expUp;}
-    protected void setExpUp(double expUp){this.expUp= expUp;}
-    protected double getExpGanho(){return  expGanho;}
-    protected void setExpGanho(double expGanho){this.expGanho = expGanho;}
+
     //Status
-    protected String getNome(){
-        return nome;
-    }
-    protected void setNome(String nome){this.nome = nome;}
-    protected String getRaca(){
-        return raca;
-    }
-    protected String getTrilha(){return trilha;}
+    protected String getRaca(){return raca;}
     protected double getVelocidade() {return velocidade;}
-    protected void setVelocidade(double valor) {this.velocidade = valor;}
+    protected void setVelocidade(double velocidade) {this.velocidade = velocidade;}
     protected double getStrength() {return strength;}
-    protected void setStrength(double valor) {this.strength = valor;}
-    protected double getVida(){return this.vida;}
-    protected void setVida(double valor) {this.vida = valor;}
+    protected void setStrength(double strength) {this.strength = strength;}
+    protected double getVidaAtual(){return this.vidaAtual;}
+
+    protected void setVidaAtual(double vida) {this.vidaAtual = vida;}
+    protected double getVidaMAx(){return this.vidaMax;}
+    protected void setVidaMax(double vidaMax) {this.vidaMax = vidaMax;}
 }
 
-class Heroi extends Personagem{
+class Heroi extends Personagem {
+    protected String nome;
+    private double expAtual = 0;
+    private double expUp = 10;
+    private String trilha;
+
     //Construtor
-    Heroi(String nome, Racas raca, Trilhas trilha){
+    Heroi(String nome, Racas raca, Trilhas trilha) {
         this.nome = nome;
-        this.vida = raca.getHpBase() * trilha.getHpMult();
+        this.vidaMax = this.vidaAtual = raca.getHpBase() * trilha.getHpMult();
         this.strength = raca.getStrengthBase() * trilha.getStrengthMult();
         this.velocidade = raca.getSpeedBase() * trilha.getSpeedMult();
         this.raca = raca.getNome();
         this.trilha = trilha.getNome();
     }
+    public String getNome(){return nome;}
+    public void setNome(String nome){this.nome = nome;}
+    public String getTrilha() {
+        return trilha;
+    }
+
+    public double getExpAtual() {
+        return expAtual;
+    }
+
+    public void setExpAtual(double ExpAtual) {
+        this.expAtual = ExpAtual;
+    }
+
+    public double getExpUp() {
+        return expUp;
+    }
+
+    public void setExpUp(double expUp) {
+        this.expUp = expUp;
+    }
+
+    public void Batalha(Heroi heroi, Inimigo inimigo) {
+
+    }
 }
+class Inimigo extends Personagem{
+    private double expDrop;
+    Inimigo(Racas raca){
+        this.vidaAtual = raca.getHpBase();
+        this.vidaMax = raca.getHpBase();
+        this.strength = raca.getStrengthBase();
+        this.velocidade = raca.getSpeedBase();
+        this.raca = raca.getNome();
+        this.expDrop = raca.getExpDrop() * this.level;
+    }
+}
+
